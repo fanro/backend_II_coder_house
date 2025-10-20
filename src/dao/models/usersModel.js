c40c9ec799fc45bce3be098d45a89df8c5d47b4a
Max Power <fanro_87@hotmail.com>
2025-10-20 09:40:06 -0300

    feat: user model

---
+++ src/dao/models/usersModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import paginate from 'mongoose-paginate-v2';
+
+const schemaUser = new mongoose.Schema(
+  {
+    first_name: String,
+    last_name: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    age: Number,
+    password: String,
+    role: {
+      type: String,
+      default: 'user',
+    },
+    cart: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'carts',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+schemaUser.plugin(paginate);
+
+const User = mongoose.model('User', schemaUser);
+
+export default User;
