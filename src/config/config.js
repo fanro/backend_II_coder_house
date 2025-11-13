7ab3627ae25ee20cc2721aef7f1160714dd3aebd
Max Power <fanro_87@hotmail.com>
2025-11-13 09:41:51 -0300

    feat: add env

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,9 @@ dotenv.config();
 
 export const config = {
   PORT: process.env.PORT || 8080,
-  MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://....',
-  DB_NAME: process.env.DB_NAME || 'DB_NAME',
-  JWT_SECRET: process.env.JWT_SECRET || 'super-clave-secreta',
+  MONGO_URL:
+    process.env.MONGO_URL ||
+    'mongodb+srv://coder:codercoder@maxpower.t5iqm.mongodb.net/?retryWrites=true&w=majority&appName=MaxPower',
+  DB_NAME: process.env.DB_NAME || 'coder_backend_I',
+  JWT_SECRET: process.env.JWT_SECRET || 'CoderCoder123',
 };
