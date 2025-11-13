403529e030c2c677192394b88128deec0520d496
Max Power <fanro_87@hotmail.com>
2025-11-13 10:23:55 -0300

    feat: session controller

---
+++ src/controllers/sessions.js
@@ -0,0 +1,73 @@
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config.js';
+
+export const sessionsController = {
+  login: (req, res, next) => {
+    passport.authenticate('login', { session: false }, (err, user, info) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(500)
+          .json({ error: info?.message || 'Error interno del servidor' });
+      }
+
+      if (!user)
+        return res
+          .status(401)
+          .json({ status: 'error', message: info?.message || 'No autorizado' });
+
+      const payload = {
+        sub: user._id,
+        email: user.email,
+        role: user.role,
+      };
+      const token = jwt.sign(payload, config.JWT_SECRET, { expiresIn: '1h' });
+
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: false,
+        maxAge: 3600000,
+      });
+
+      const { password, ...userSafe } = user;
+      res.json({
+        status: 'success',
+        message: 'Login exitoso',
+        user: userSafe,
+      });
+    })(req, res, next);
+  },
+
+  register: (req, res, next) => {
+    passport.authenticate('register', { session: false }, (err, user, info) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ error: info?.message || 'Error interno del servidor' });
+      }
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: info.message || 'Error en el registro' });
+      }
+
+      const { password, ...userSafe } = user.toObject();
+      res.status(201).json({ message: 'Usuario registrado', user: userSafe });
+    })(req, res, next);
+  },
+
+  current: [
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      return res.status(200).json({ payload: req.user });
+    },
+  ],
+
+  logout: (req, res) => {
+    res.clearCookie('jwt');
+    return res.status(200).json({ message: 'Usuario deslogueado' });
+  },
+};
--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -1,76 +1,14 @@
 import express from 'express';
-import { config } from '../config/config.js';
-import jwt from 'jsonwebtoken';
-import passport from 'passport';
+import { sessionsController } from '../controllers/sessions.js';
 
 const router = express.Router();
 
-router.post('/register', (req, res, next) => {
-  passport.authenticate('register', { session: false }, (err, user, info) => {
-    if (err) {
-      return res
-        .status(500)
-        .json({ error: info?.message || 'Error interno del servidor' });
-    }
+router.post('/register', sessionsController.register);
 
-    if (!user) {
-      return res
-        .status(400)
-        .json({ error: info.message || 'Error en el registro' });
-    }
+router.post('/login', sessionsController.login);
 
-    const { password, ...userSafe } = user.toObject();
-    res.status(201).json({ message: 'Usuario registrado', user: userSafe });
-  })(req, res, next);
-});
+router.get('/current', sessionsController.current);
 
-router.post('/login', (req, res, next) => {
-  passport.authenticate('login', { session: false }, (err, user, info) => {
-    if (err) {
-      return res
-        .status(500)
-        .json({ error: info?.message || 'Error interno del servidor' });
-    }
-
-    if (!user)
-      return res
-        .status(401)
-        .json({ status: 'error', message: info?.message || 'No autorizado' });
-
-    const payload = {
-      sub: user._id,
-      email: user.email,
-      role: user.role,
-    };
-    const token = jwt.sign(payload, config.JWT_SECRET, { expiresIn: '1h' });
-
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: false,
-      maxAge: 3600000,
-    });
-
-    const { password, ...userSafe } = user;
-    res.json({
-      status: 'success',
-      message: 'Login exitoso',
-      user: userSafe,
-    });
-  })(req, res, next);
-});
-
-router.get(
-  '/current',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    return res.status(200).json({ payload: req.user });
-  }
-);
-
-router.get('/logout', (req, res) => {
-  res.clearCookie('jwt');
-  return res.status(200).json({ message: 'Usuario deslogueado' });
-});
+router.get('/logout', sessionsController.logout);
 
 export default router;
